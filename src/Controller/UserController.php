33e010de13cbad65b4c21d1fe2009b4098874ba4
Jiwiin <145154194+Jiwiin@users.noreply.github.com>
2024-03-28 14:54:55 +0100

    "Gerer son profil" terminé modif Usercontroller et Edit.html.twig

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/user')]
@@ -51,12 +52,19 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Vérifier si un nouveau mot de passe a été soumis
+            $newPassword = $form->get('password')->getData();
+            if ($newPassword) {
+                // Hasher le nouveau mot de passe
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $newPassword);
+                $user->setPassword($hashedPassword);
+            }
             $entityManager->flush();
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
