f7e2bc6119a2d5241f47680d290fbf3415e50ccc
Jiwiin <145154194+Jiwiin@users.noreply.github.com>
2024-03-31 00:35:29 +0100

    Update on admin part : add list of users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,10 +19,17 @@ class AdminController extends AbstractController
     }
 
     #[Route('/users', name: 'app_admin_show',  methods: ['GET','POST'])]
-    public function show(User $user): Response
+    public function show(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
         return $this->render('admin/show.html.twig',  [
-            'user' => $user,
+            'users' => $users,
         ]);
+    }
+
+    #[Route('/users/{id}', name: 'app_admin_edit',  methods: ['GET','POST'])]
+    public function edit(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/edit.html.twig');
     }
 }
