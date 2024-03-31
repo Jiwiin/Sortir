c34580a68cd8fd80a171504ba1d26b41f6b9a974
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-03-31 15:12:18 +0200

    Updated EventRepository filter for admin

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -110,4 +110,74 @@ class EventRepository extends ServiceEntityRepository
         return $query->getQuery()->getResult();
     }
 
+    public function findSearchAdmin(SearchData $search): array
+    {
+        $query = $this
+            ->createQueryBuilder('e')
+            ->join('e.eventOrganizer', 'o')
+            ->join('o.campus', 'c');
+
+        if(!empty($search->q)) {
+            $query
+                ->andWhere('e.name LIKE :q')
+                ->setParameter('q', "%{$search->q}%");
+        }
+
+        if(!empty($search->campus)) {
+            $query
+                ->andWhere('c.id = :campusId')
+                ->setParameter('campusId', $search->campus);
+        }
+
+        if(!empty($search->startDate)) {
+            $query
+                ->andWhere('e.dateStart >= :startDate')
+                ->setParameter('startDate', $search->startDate);
+        }
+
+        if(!empty($search->endDate)) {
+            $query
+                ->andWhere('e.dateStart <= :endDate')
+                ->setParameter('endDate', $search->endDate);
+        }
+
+        if($search->organized) {
+            $query
+                ->andWhere('e.eventOrganizer = :organizer')
+                ->setParameter('organizer', $search->user);
+        }
+
+        if($search->registered) {
+            $query
+                ->andWhere(':user MEMBER OF e.participate')
+                ->setParameter('user', $search->user);
+        }
+
+        if ($search->notRegistered) {
+            $query
+                ->andWhere(':user NOT MEMBER OF e.participate')
+                ->setParameter('user', $search->user);
+        }
+
+        if($search->eventCompleted) {
+            $query
+                ->andWhere('e.dateStart < :now')
+                ->setParameter('now', new \DateTime());
+        }
+        //Ordre affichage
+        $query
+            ->addSelect("
+        CASE
+            WHEN e.eventOrganizer = :user THEN 1
+            WHEN :user MEMBER OF e.participate THEN 2
+            ELSE 3
+        END AS HIDDEN priority
+    ")
+            ->setParameter('user', $search->user)
+            ->orderBy('priority', 'ASC')
+            ->addOrderBy('e.dateLimitRegistration', 'ASC');
+
+        return $query->getQuery()->getResult();
+    }
+
 }
