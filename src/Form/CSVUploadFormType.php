065af2b8d57505c81e7807a04f6ff9dcd2fbeb4c
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-04-03 12:04:26 +0200

    Updated login form

---
+++ src/Form/CSVUploadFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+
+class CSVUploadFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('csvFile', FileType::class, [
+                'label' => 'Fichier CSV (CSV file)',
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'text/csv',
+                            'text/plain',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader un fichier CSV valide.',
+                    ])
+                ],
+            ]);
+    }
+}
