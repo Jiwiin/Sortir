823c89502314e995fe6ef118f30015c44c59ec2c
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-04-04 16:54:02 +0200

    Updated creation new location

---
+++ src/Form/LocationType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Location;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du lieu',
+                'required' => true,
+            ])
+            ->add('street', TextType::class, [
+                'label' => 'Adresse',
+                'required' => true,
+            ])
+            ->add('latitude', TextType::class, [
+                'label' => 'Latitude',
+                'required' => false,
+            ])
+            ->add('longitude', TextType::class, [
+                'label' => 'Longitude',
+                'required' => false,
+            ])
+            ->add('city', EntityType::class, [
+                'label' => 'Ville',
+                'choice_label' => 'name',
+                'class' => City::class,
+                'required' => true,
+            ])
+            ->add('add', SubmitType::class, [
+                'label'=> 'Ajouter',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
